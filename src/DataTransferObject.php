b0ec5a831b8dbf8c4d97a0da960d5ffd0399a9d5
Vladislav <raytracer@yandex.ru>
2020-05-05 16:51:09 +0500

    Release

---
+++ src/DataTransferObject.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace mrln\DataTransfer;
+
+
+use ReflectionClass;
+use ReflectionProperty;
+
+abstract class DataTransferObject
+{
+    public function __construct(array $parameters = [])
+    {
+        $class = new ReflectionClass(static::class);
+
+        foreach ($class->getProperties(ReflectionProperty::IS_PUBLIC) as $reflectionProperty){
+            $property = $reflectionProperty->getName();
+            $converted = $this->camelCaseTo_snake_case($property);
+            $this->{$property} = $parameters[$converted];
+        }
+    }
+
+    private function camelCaseTo_snake_case(string $input) {
+        return strtolower(
+            preg_replace(
+                '/([^A-Z])([A-Z])/',
+                "$1_$2",
+                $input
+            )
+        );
+    }
+}
