9b0455c1dce9fd0932cbf015fbf501455cd27e79
Vladislav <raytracer@yandex.ru>
2020-05-06 19:57:51 +0500

    Added messageId property

--- src/DataTransferObject.php
+++ src/DataTransferObject.php
@@ -9,11 +9,13 @@ use ReflectionProperty;
 
 abstract class DataTransferObject
 {
+    private $messageId;
+
     public function __construct(array $parameters = [])
     {
         $class = new ReflectionClass(static::class);
 
-        foreach ($class->getProperties(ReflectionProperty::IS_PUBLIC) as $reflectionProperty){
+        foreach ($class->getProperties(ReflectionProperty::IS_PUBLIC) as $reflectionProperty) {
             $property = $reflectionProperty->getName();
             $converted = $this->camelCaseTo_snake_case($property);
             if (!empty($parameters[$converted])) {
@@ -22,7 +24,20 @@ abstract class DataTransferObject
         }
     }
 
-    private function camelCaseTo_snake_case(string $input) {
+    public function getMessageId(string $id)
+    {
+        return $this->messageId;
+    }
+
+    public function setMessageId(string $id): self
+    {
+        $this->messageId = $id;
+
+        return $this;
+    }
+
+    private function camelCaseTo_snake_case(string $input)
+    {
         return strtolower(
             preg_replace(
                 '/([^A-Z])([A-Z])/',
