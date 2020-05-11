7ae5167ec9c5ba955ae5bf71be8e011d109215e4
Vladislav <raytracer@yandex.ru>
2020-05-11 11:11:43 +0500

    Revert back, it's not dto business

--- src/DataTransferObject.php
+++ src/DataTransferObject.php
@@ -9,15 +9,6 @@ use ReflectionProperty;
 
 abstract class DataTransferObject
 {
-    // correlation id - unique identifier that indicates which request message this reply is for
-    public $__cid;
-    // transaction id - unique identifier of first message in the chain
-    public $__tid;
-    // the name of the service emitting the event.
-    public $__emit;
-    // The name of the service consuming the event.
-    public $__consume;
-
     public function __construct(array $parameters = [])
     {
         $class = new ReflectionClass(static::class);
@@ -29,54 +20,6 @@ abstract class DataTransferObject
                 $this->{$property} = $parameters[$converted];
             }
         }
-    }
-
-    public function getCorrelationId(): ?string
-    {
-        return $this->__cid;
-    }
-
-    public function setCorrelationId($cid): self
-    {
-        $this->__cid = $cid;
-
-        return $this;
-    }
-
-    public function getTransactionId(): ?string
-    {
-        return $this->__tid;
-    }
-
-    public function setTransactionId($tid): self
-    {
-        $this->__tid = $tid;
-
-        return $this;
-    }
-
-    public function getWhoEmitted(): ?string
-    {
-        return $this->__emit;
-    }
-
-    public function setWhoEmitted($emit): self
-    {
-        $this->__emit = $emit;
-
-        return $this;
-    }
-
-    public function getWhoConsumed(): ?string
-    {
-        return $this->__consume;
-    }
-
-    public function setWhoConsumed(string $consume): self
-    {
-        $this->__consume = $consume;
-
-        return $this;
     }
 
     private function camelCaseTo_snake_case(string $input)
